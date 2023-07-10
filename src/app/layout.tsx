b59dceee27304589ea1eeadcbe65601760acddf9
Filip Heinel <77172584+Wilkrew@users.noreply.github.com>
2023-07-10 21:43:23 +0200

    Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
-import Providers from "@/components/Providers";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import Header from "@/components/Header";
-import { ThemeOptions } from "@mui/material";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
